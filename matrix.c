28948563a94b2b43ade75ed539e70346c4be477b
Kyle Wood <demonwav@gmail.com>
2017-10-26 04:17:20 -0500

    Use CLOCK_MONOTONIC_RAW for macOS instead

--- matrix.c
+++ matrix.c
@@ -4,28 +4,13 @@
 #include <omp.h>
 #include "matrix.h"
 
-#ifdef __MACH__
-#include <mach/clock.h>
-#include <mach/mach.h>
-#endif
-
 double** generate_random_matrix(int dim) {
     printf("Generating matrix (%i x %i)...\n", dim, dim);
     double** result = create_matrix(dim);
 
     struct timespec ts;
 
-#ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
-    clock_serv_t cclock;
-    mach_timespec_t mts;
-    host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
-    clock_get_time(cclock, &mts);
-    mach_port_deallocate(mach_task_self(), cclock);
-    ts.tv_sec = mts.tv_sec;
-    ts.tv_nsec = mts.tv_nsec;
-#else
-    clock_gettime(CLOCK_REALTIME, &ts);
-#endif
+    clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
 
     srandom((unsigned int) (ts.tv_nsec ^ ts.tv_sec));
 
